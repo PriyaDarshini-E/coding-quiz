b0c7d4aef16af0f3ce936cec1eda91149cda24f7
PriyaDarshini-E <193783483+PriyaDarshini-E@users.noreply.github.com.>
2025-12-28 16:58:50 -0500

    wcq completed

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ import Bootstrap_begin from "./components/Bootstrap_begin";
 import Bootstrap_advan from "./components/Bootstrap_advan";
 import Bootstrap_inter from "./components/Bootstrap_inter";
 
-import Javascript from "./components/javascript";
-import Javascript_begin from "./components/javascript_begin";
-import Javascript_inter from "./components/javascript_inter";
-import Javascript_advan from "./components/javascript_advan";
+import Javascript from "./components/Javascript";
+import Javascript_begin from "./components/JavaScript_begin";
+import Javascript_inter from "./components/JavaScript_inter";
+import Javascript_advan from "./components/JavaScript_advan";
 
 import Sql from "./components/Sql";
 import Sql_begin from "./components/Sql_begin";
